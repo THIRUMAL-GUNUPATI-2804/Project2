3e70d076451de401dcf4bd25551f8f301ef223fd
THIRUMAL-GUNUPATI-2804 <96756060+THIRUMAL-GUNUPATI-2804@users.noreply.github.com>
2023-06-29 19:28:57 +0530

    stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,78 +1,72 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+var msec=0;
+var sec=0;
+var min=0;
+var d=0;
+var e=0;
+var z=0;
+var check=true;
+var clear=0;
+var stopwatch=document.getElementById('display');
+stopwatch.innerHTML='00:00:00';
+function start(){
+    msec=addZeroMsec(msec+1);
+    if(msec>99){
+        sec=addZeroSec(sec+1);
+        msec=addZeroMsec(0);
+    }
+    if(sec>59){
+        sec=addZeroSec(0);
+        msec=addZeroMsec(0);
+        min=addZeroMin(min+1);
+    }
+    stopwatch.innerHTML=''+z+min+':'+e+sec+':'+d+msec;
+}
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
-
-startBtn.addEventListener('click', function () {
-	timer = true;
-	stopWatch();
-});
-
-stopBtn.addEventListener('click', function () {
-	timer = false;
-});
-
-resetBtn.addEventListener('click', function () {
-	timer = false;
-	hour = 0;
-	minute = 0;
-	second = 0;
-	count = 0;
-	document.getElementById('hr').innerHTML = "00";
-	document.getElementById('min').innerHTML = "00";
-	document.getElementById('sec').innerHTML = "00";
-	document.getElementById('count').innerHTML = "00";
-});
-
-function stopWatch() {
-	if (timer) {
-		count++;
-
-		if (count == 100) {
-			second++;
-			count = 0;
-		}
-
-		if (second == 60) {
-			minute++;
-			second = 0;
-		}
-
-		if (minute == 60) {
-			hour++;
-			minute = 0;
-			second = 0;
-		}
-
-		let hrString = hour;
-		let minString = minute;
-		let secString = second;
-		let countString = count;
-
-		if (hour < 10) {
-			hrString = "0" + hrString;
-		}
-
-		if (minute < 10) {
-			minString = "0" + minString;
-		}
-
-		if (second < 10) {
-			secString = "0" + secString;
-		}
-
-		if (count < 10) {
-			countString = "0" + countString;
-		}
-
-		document.getElementById('hr').innerHTML = hrString;
-		document.getElementById('min').innerHTML = minString;
-		document.getElementById('sec').innerHTML = secString;
-		document.getElementById('count').innerHTML = countString;
-		setTimeout(stopWatch, 10);
-	}
+function initiate(){
+    if(check===true){
+        check=false;
+        clear=setInterval(start,10);
+    }
+}
+function stop(){
+    check=true;
+    clearInterval(clear);
+}
+function reset(){
+    msec=0;
+    sec=0;
+    min=0;
+    check=true;
+    clearInterval(clear);
+    stopwatch.innerHTML='00:00:00';
+}
+function addZeroMsec(time){
+    var length= time.toString().length;
+    if(length<2){
+        d=0;
+    }
+    else{
+        d='';
+    }
+    return time;
+}
+function addZeroSec(time){
+    var length=time.toString().length;
+    if(length<2){
+        e=0;
+    }
+    else{
+        e='';
+    }
+    return time;
+}
+function addZeroMin(time){
+    var length=time.toString().length;
+    if(length<2){
+        z=0;
+    }
+    else{
+        z='';
+    }
+    return time;
 }
